8dac8af0b2945de8673f866d2d2d67d5eb493787
Carl-Johan Kjellander <carl-johan.kjellander@kivra.com>
2024-10-09 11:20:29 +0200

    feat: implement radix64:encode/1

--- src/radix64.erl
+++ src/radix64.erl
@@ -1,6 +1,7 @@
 -module(radix64).
 
 -export([ decode/1
+        , encode/1
         , crc24/1
         ]).
 
@@ -54,6 +55,35 @@ decode_binary(<<C4:8, Cs/bits>>, A, B1, B2, B3) ->
         eq                -> decode_binary(Cs, <<A/bits,B1:6,B2:6,(B3 bsr 2):4>>);
         B4                -> decode_binary(Cs, <<A/bits,B1:6,B2:6,B3:6,B4:6>>)
     end.
+
+encode(Bin) when is_binary(Bin) ->
+    B64 = encode_binary(Bin, <<>>),
+    Split64 = split_lines(B64),
+    CRC24 = encode_binary(crc24(Bin), <<>>),
+    iolist_to_binary([Split64, <<"=">>, CRC24, <<"\n">>]).
+
+encode_binary(<<>>, A) ->
+    A;
+encode_binary(<<B1:8>>, A) ->
+    <<A/bits,(b64e(B1 bsr 2)):8,(b64e((B1 band 3) bsl 4)):8,$=:8,$=:8>>;
+encode_binary(<<B1:8, B2:8>>, A) ->
+    <<A/bits,(b64e(B1 bsr 2)):8,
+      (b64e(((B1 band 3) bsl 4) bor (B2 bsr 4))):8,
+      (b64e((B2 band 15) bsl 2)):8, $=:8>>;
+encode_binary(<<B1:8, B2:8, B3:8, Ls/bits>>, A) ->
+    BB = (B1 bsl 16) bor (B2 bsl 8) bor B3,
+    encode_binary(Ls,
+                  <<A/bits,(b64e(BB bsr 18)):8,
+                    (b64e((BB bsr 12) band 63)):8,
+                    (b64e((BB bsr 6) band 63)):8,
+                    (b64e(BB band 63)):8>>).
+
+split_lines(<<H:64/binary,T/binary>>) ->
+    [H, <<"\n">>] ++ split_lines(T);
+split_lines(<<>>) ->
+    [];
+split_lines(Last) when is_binary(Last) ->
+    [Last, <<"\n">>].
 
 %% ### 6.1.  An Implementation of the CRC-24 in "C"
 %%     #define CRC24_INIT 0xB704CEL
@@ -132,14 +162,14 @@ b64d(X) ->
              bad,bad,bad,bad,bad,bad,bad,bad,bad,bad,bad,bad,bad,bad,bad,bad,
              bad,bad,bad,bad,bad,bad,bad,bad,bad,bad,bad,bad,bad,bad,bad,bad}).
 
-%% -compile({inline, [{b64e, 1}]}).
-%% b64e(X) ->
-%%     element(X+1,
-%% 	    {$A, $B, $C, $D, $E, $F, $G, $H, $I, $J, $K, $L, $M, $N,
-%% 	     $O, $P, $Q, $R, $S, $T, $U, $V, $W, $X, $Y, $Z,
-%% 	     $a, $b, $c, $d, $e, $f, $g, $h, $i, $j, $k, $l, $m, $n,
-%% 	     $o, $p, $q, $r, $s, $t, $u, $v, $w, $x, $y, $z,
-%% 	     $0, $1, $2, $3, $4, $5, $6, $7, $8, $9, $+, $/}).
+-compile({inline, [{b64e, 1}]}).
+b64e(X) ->
+    element(X+1,
+            {$A, $B, $C, $D, $E, $F, $G, $H, $I, $J, $K, $L, $M, $N,
+             $O, $P, $Q, $R, $S, $T, $U, $V, $W, $X, $Y, $Z,
+             $a, $b, $c, $d, $e, $f, $g, $h, $i, $j, $k, $l, $m, $n,
+             $o, $p, $q, $r, $s, $t, $u, $v, $w, $x, $y, $z,
+             $0, $1, $2, $3, $4, $5, $6, $7, $8, $9, $+, $/}).
 
 %%%========================================================================
 %%% eunit
