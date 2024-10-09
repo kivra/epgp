15e3375e41f4036090b223701c86af75163616d9
Carl-Johan Kjellander <carl-johan.kjellander@kivra.com>
2024-10-09 17:12:03 +0200

    feat: implement symetric encryption

--- src/epgp.erl
+++ src/epgp.erl
@@ -2,6 +2,7 @@
 
 -export([ parse/1
         , parse/2
+        , sym_encrypt/2
         , testmsg/0
         ]).
 
@@ -34,6 +35,96 @@
 -record(pgp_ctx, { pw_fun
                  , skey
                  }).
+
+sym_encrypt(Message, Password) when is_binary(Message) andalso
+                                    is_binary(Password) ->
+    case {unicode:characters_to_nfc_binary(Message),
+          unicode:characters_to_nfc_binary(Password)} of
+        {{error, _, _}, _} -> {error, non_utf8_message};
+        {_, {error, _, _}} -> {error, non_utf8_password};
+        {B1, B2} when is_binary(B1) andalso
+                      is_binary(B2) ->
+            PassFun = fun () -> Password end,
+            ArmorR64 = do_sym_encrypt(Message,
+                                      #pgp_ctx{ pw_fun = PassFun }),
+            <<"-----BEGIN PGP MESSAGE-----\n\n", ArmorR64/binary,
+              "-----END PGP MESSAGE-----\n">>
+    end.
+
+do_sym_encrypt(Message, Ctx0) ->
+    {SeshKeyData, Ctx1} = pack_ske_skey(Ctx0),
+    LitData = pack_lit_data(utf8, Message),
+    CompData = pack_comp_data(zip, LitData),
+    EncData = pack_se_n_ip_data(CompData, Ctx1),
+    radix64:encode(<<SeshKeyData/binary, EncData/binary>>).
+
+pack_ske_skey(#pgp_ctx{pw_fun = PassFun} = Ctx) ->
+    CountField = 255,
+    SymAlg = aes_256_cfb128,
+    case {crypto:strong_rand_bytes(8), crypto:strong_rand_bytes(32)} of
+        {{error, Reason}, _} ->
+            error(Reason);
+        {_, {error, Reason}} ->
+            error(Reason);
+        {Salt, SeshKey0} ->
+            SKey = s2k(iter_salted_s2k, CountField, PassFun, Salt,
+                       sha256),
+            S2KTag = s2k_tag(iter_salted_s2k),
+            CryptTag = sym_tag(SymAlg),
+            SeshKey1 = <<CryptTag, SeshKey0/binary>>,
+            EncSeshKey = crypto:crypto_one_time(SymAlg, SKey, iv(SymAlg),
+                                                SeshKey1, true),
+            HashTag = hash_tag(sha256),
+            CtxSesh = #pgp_skey{ alg = SymAlg
+                               , skey_fun = fun () -> SeshKey0 end
+                               },
+            {packet(3, <<4,CryptTag,S2KTag,HashTag,Salt/binary,CountField,
+                         EncSeshKey/binary>>),
+             Ctx#pgp_ctx{skey = CtxSesh}}
+    end.
+
+pack_lit_data(utf8, Data) ->
+    UnixTs = unix_ts_32(),
+    %% Filename length = 0, no filename
+    packet(11, <<$u, 0, UnixTs/binary, Data/binary>>).
+
+pack_comp_data(zip, Data) ->
+    Z = zlib:open(),
+    zlib:deflateInit(Z, default, deflated, -13, 8, default),
+    [Zip] = zlib:deflate(Z, Data, finish),
+    zlib:close(Z),
+    Tag = comp_tag(zip),
+    packet(8, <<Tag, Zip/binary>>).
+
+pack_se_n_ip_data(Data, #pgp_ctx{skey = #pgp_skey{ alg = SeshAlg
+                                                 , skey_fun = SKeyFun
+                                                 }}) ->
+    case crypto:strong_rand_bytes(bsize(SeshAlg)) of
+        {error, Reason} ->
+            error(Reason);
+        Nonce ->
+            ChSum = binary:part(Nonce, bsize(SeshAlg)-2, 2),
+            IVData = <<Nonce/binary, ChSum/binary, Data/binary,
+                       16#d3, 16#14>>,
+            Digest = crypto:hash(sha, IVData),
+            Plain = <<IVData/binary, Digest/binary>>,
+            Encrypted = crypto:crypto_one_time(SeshAlg, SKeyFun(),
+                                               iv(SeshAlg),
+                                               Plain, true),
+            packet(18, <<1, Encrypted/binary>>)
+    end.
+
+packet(Tag, Message) when Tag >= 0 andalso Tag < 64->
+    Len = packet_len(byte_size(Message)),
+    <<1:1,1:1,Tag:6,Len/binary,Message/binary>>.
+
+packet_len(N) when N >= 0 andalso N < 192 ->
+    <<N>>;
+packet_len(N) when N >= 192 andalso N < 8384 ->
+    <<N0, N1>> = <<(N - 192):16>>,
+    <<(N0 + 192), N1>>;
+packet_len(N) when N >= 8384  ->
+    <<255, N:32/big-unsigned-integer>>.
 
 parse(Message) when is_binary(Message) ->
     do_parse(Message, #pgp_ctx{}).
@@ -98,7 +189,7 @@ find_length_new(<<L,R0/binary>>, Tag, Ctx0) when L < 192 ->
     [Res] ++ parse_packets(R1, Ctx1);
 find_length_new(<<L0,L1,R0/binary>>, Tag, Ctx) when L0 >= 192 andalso
                                                     L0 < 224 ->
-    L = (L0 - 192) bsl 8 + L1,
+    L = (L0 - 192) bsl 8 + L1 + 192,
     <<Packet:L/binary,R1/binary>> = R0,
     [{Tag, L, Packet}] ++ parse_packets(R1, Ctx);
 find_length_new(<<255,L:32/big-unsigned-integer,R0/binary>>, Tag,
@@ -122,15 +213,8 @@ parse_ske_skey(<<4,SymAlg,S2K,R/binary>>, Rec, Ctx) ->
 
 s2k_hash(iter_salted_s2k, <<Hash,Salt:8/binary,C,R/binary>>,
          SymAlg, Rec0, #pgp_ctx{pw_fun = PassFun} = Ctx) ->
-    <<Exp:4, C1:4>> = <<C>>,
-    Count = (16 + C1) bsl (Exp + ?EXPBIAS),
-    Password = PassFun(),
     HashAlg = hash_alg(Hash),
-    SaltedPw = <<Salt/binary, Password/binary>>,
-    IterC = Count div byte_size(SaltedPw) + 1,
-    IterPre = binary:copy(SaltedPw, IterC),
-    Iter = binary:part(IterPre, 0, Count),
-    SKey = crypto:hash(HashAlg, Iter),
+    SKey = s2k(iter_salted_s2k, C, PassFun, Salt, HashAlg),
     <<SAlg, Decrypted/binary>> =
         crypto:crypto_one_time(SymAlg, SKey, iv(SymAlg), R, false),
     SeshAlg = sym_alg(SAlg),
@@ -141,9 +225,25 @@ s2k_hash(iter_salted_s2k, <<Hash,Salt:8/binary,C,R/binary>>,
     SRec = #pgp_skey{ alg = SeshAlg
                     , skey_fun = SKeyFun
                     },
-    {{iter_salted_s2k, HashAlg, Salt, Count, SymAlg, SKey, R, Decrypted,
+    {{iter_salted_s2k, HashAlg, Salt, SymAlg, SKey, R, Decrypted,
       Rec1},
      Ctx#pgp_ctx{skey = SRec}}.
+
+s2k(iter_salted_s2k, CountField, PassFun, Salt, HashAlg) ->
+    Res = do_s2k(iter_salted_s2k, CountField, PassFun, Salt, HashAlg),
+    erlang:garbage_collect(),
+    Res.
+
+do_s2k(iter_salted_s2k, CountField, PassFun, Salt, HashAlg) ->
+    <<Exp:4, C1:4>> = <<CountField>>,
+    Count = (16 + C1) bsl (Exp + ?EXPBIAS),
+    Password = PassFun(),
+    SaltedPw = <<Salt/binary, Password/binary>>,
+    IterC = Count div byte_size(SaltedPw) + 1,
+    IterPre = binary:copy(SaltedPw, IterC),
+    Iter = binary:part(IterPre, 0, Count),
+    SKey = crypto:hash(HashAlg, Iter),
+    SKey.
 
 parse_se_n_ip_data(<<1,Encrypted/binary>>, _Rec,
                    #pgp_ctx{skey = #pgp_skey{ alg = SeshAlg
@@ -188,6 +288,8 @@ parse_lit_data(<<$u, L, Utf8/binary>>, _Rec, Ctx) ->
 
 sym_alg(9) -> aes_256_cfb128.
 
+sym_tag(aes_256_cfb128) -> 9.
+
 iv(aes_256_cfb128) -> <<0:128>>.
 
 bsize(aes_256_cfb128) -> 16.
@@ -195,6 +297,10 @@ bsize(aes_256_cfb128) -> 16.
 s2k_spec(0) -> simple_s2k;
 s2k_spec(1) -> salted_s2k;
 s2k_spec(3) -> iter_salted_s2k.
+
+s2k_tag(simple_s2k) -> 0;
+s2k_tag(salted_s2k) -> 1;
+s2k_tag(iter_salted_s2k) -> 3.
 
 hash_alg(1) -> md5;
 hash_alg(2) -> sha;
@@ -204,10 +310,28 @@ hash_alg(9) -> sha384;
 hash_alg(10) -> sha512;
 hash_alg(11) -> sha224.
 
+hash_tag(md5) -> 1;
+hash_tag(sha) -> 2;
+hash_tag(ripemd160) -> 3;
+hash_tag(sha256) -> 8;
+hash_tag(sha384) -> 9;
+hash_tag(sha512) -> 10;
+hash_tag(sha224) -> 11.
+
 comp_alg(0) -> none;
 comp_alg(1) -> zip;
 comp_alg(2) -> zlib;
 comp_alg(3) -> bzip2.
+
+comp_tag(zip) -> 1.
+
+unix_ts_32() ->
+    UnixTS =
+        calendar:datetime_to_gregorian_seconds(
+          calendar:universal_time()) -
+        calendar:datetime_to_gregorian_seconds(
+          {{1970,1,1},{0,0,0}}),
+    <<UnixTS:32>>.
 
 testmsg() ->
     <<"-----BEGIN PGP MESSAGE-----\n"
